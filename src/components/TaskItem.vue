7435dfd7a70f0c411269d43b36d7deaf7ec52405
Asef00 <asefhosseini00@gmail.com>
2023-09-18 19:01:57 +0400

    configure styles for task action icons

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="flex justify-between px-4 py-5 shadow bg-slate-600 rounded">
+  <div class="flex justify-between items-center px-4 py-5 shadow bg-slate-600 rounded">
     <p>{{ props.task.title }}</p>
     <div class="flex gap-2">
-      <i class="material-icons">delete</i>
-      <i class="material-icons">favorite</i>
+      <button><i class="material-icons hover:text-gray-50 drop-shadow">delete</i></button>
+      <button>
+        <i
+          class="material-icons hover:text-gray-50 drop-shadow"
+          :class="{ 'text-red-600': props.task.isFav }"
+        >
+          favorite
+        </i>
+      </button>
     </div>
   </div>
 </template>
