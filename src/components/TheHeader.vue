3baa3bbd25ff8a64052278a65a74979624f0275b
Asef00 <asefhosseini00@gmail.com>
2023-09-18 14:35:16 +0400

    create sample tasks on home page

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,22 +3,20 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <div class="flex flex-col gap-4 items-center">
-        <div class="flex items-end gap-2">
-          <img class="-rotate-12" src="@/assets/logo.svg" alt="pinia logo" width="45" height="75">
-          <h1 class="rotate-3 text-2xl font-bold mb-2">Pinia Tasks</h1>
-        </div>
-        <div class="flex gap-2">
-          <input class="rounded px-2 py-1" type="text" placeholder="I need to...">
-          <button class="rounded px-4 bg-amber-300 text-slate-950">Add</button>
-        </div>
+  <header class="mb-4">
+    <div class="flex flex-col gap-4 items-center">
+      <div class="flex items-end gap-2">
+        <img class="-rotate-12" src="@/assets/logo.svg" alt="pinia logo" width="45" height="75" />
+        <h1 class="rotate-3 text-2xl font-bold mb-2">Pinia Tasks</h1>
       </div>
-      <nav class="flex gap-2 justify-end">
-        <RouterLink class="border px-2 py-1 rounded" to="/">All Tasks</RouterLink>
-        <RouterLink class="border px-2 py-1 rounded" to="/about">Favourites</RouterLink>
-      </nav>
+      <div class="flex gap-2">
+        <input class="rounded px-2 py-1" type="text" placeholder="I need to..." />
+        <button class="rounded px-4 bg-amber-300 text-slate-950">Add</button>
+      </div>
     </div>
+    <nav class="flex gap-2 justify-end">
+      <RouterLink class="border px-2 py-1 rounded" to="/">All Tasks</RouterLink>
+      <RouterLink class="border px-2 py-1 rounded" to="/about">Favourites</RouterLink>
+    </nav>
   </header>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const taskList = [
+  { title: 'play gloomhaven', isFav: true },
+  { title: 'mow the lawn', isFav: false },
+  { title: 'become an eldern lord', isFav: true },
+  { title: 'eat some pinapples', isFav: false }
+]
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <div class="flex flex-col gap-3">
+      <div class="flex justify-between px-4 py-5 shadow bg-slate-600 rounded" v-for="(task, index) in taskList" :key="index">
+        <p>{{ task.title }}</p>
+        <div class="flex gap-2"></div>
+      </div>
+    </div>
+  </main>
 </template>
