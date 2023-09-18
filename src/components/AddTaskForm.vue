c08bacffdecd79d0c9e4887f5d159fa4ca676a9d
Asef00 <asefhosseini00@gmail.com>
2023-09-18 22:50:56 +0400

    task form component

---
+++ src/components/AddTaskForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <form class="flex gap-2" @submit.prevent="addTask">
+    <input
+      v-model="newTask"
+      class="rounded text-slate-900 px-2 py-1"
+      type="text"
+      placeholder="I need to..."
+    />
+    <button type="submit" class="rounded px-4 bg-amber-300 text-slate-950">Add</button>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useTaskStore } from '@/stores/TaskStore'
+
+const newTask = ref('')
+const taskStore = useTaskStore()
+
+const addTask = () => {
+  taskStore.addTask({ id: taskStore.totalCount + 1, title: newTask.value, isFav: false })
+  newTask.value = ''
+}
+</script>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,17 +1,9 @@
 <script setup lang="ts">
 import { useTaskStore } from '@/stores/TaskStore'
-import { ref } from 'vue'
-
+import AddTaskForm from '@/components/AddTaskForm.vue'
 const activeClass = 'bg-lime-500 text-black border-black'
 
 const taskStore = useTaskStore()
-
-const newTask = ref('')
-
-const addTask = () => {
-  taskStore.addTask({ id: taskStore.totalCount + 1, title: newTask.value, isFav: false })
-  newTask.value = ''
-}
 </script>
 
 <template>
@@ -21,15 +13,7 @@ const addTask = () => {
         <img class="-rotate-12" src="@/assets/logo.svg" alt="pinia logo" width="45" height="75" />
         <h1 class="rotate-3 text-2xl font-bold mb-2">Pinia Tasks</h1>
       </div>
-      <form class="flex gap-2" @submit.prevent="addTask">
-        <input
-          v-model="newTask"
-          class="rounded text-slate-900 px-2 py-1"
-          type="text"
-          placeholder="I need to..."
-        />
-        <button type="submit" class="rounded px-4 bg-amber-300 text-slate-950">Add</button>
-      </form>
+      <AddTaskForm />
     </div>
     <nav class="flex gap-2 justify-end">
       <button
