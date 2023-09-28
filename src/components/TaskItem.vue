27bb791c65b0813b28c6d0f845b5934327db9ea9
Asef00 <asefhosseini00@gmail.com>
2023-09-29 01:04:36 +0400

    change fav hover effect fix task id undefined ts error

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -2,12 +2,12 @@
   <div class="flex justify-between items-center px-4 py-5 shadow bg-slate-600 rounded">
     <p>{{ props.task.title }}</p>
     <div class="flex gap-2">
-      <button @click="taskStore.deleteTask(task.id)">
+      <button @click="taskStore.deleteTask(task.id!)">
         <i class="material-icons hover:text-gray-50 drop-shadow">delete</i>
       </button>
-      <button @click="taskStore.toggleFav(task.id)">
+      <button @click="taskStore.toggleFav(task.id!)">
         <i
-          class="material-icons hover:text-gray-50 drop-shadow"
+          class="material-icons hover:brightness-75 drop-shadow"
           :class="{ 'text-red-600': props.task.isFav }"
         >
           favorite
